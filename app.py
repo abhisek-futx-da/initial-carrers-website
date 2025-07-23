379b7dc03a62ad3a549200f9339569555c49c71d
abhisek-futx-da <abhishekjindal4340@gmail.com>
2025-07-23 19:43:12 +0000

    Changed the name

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ JOBS = [
 def hello_jovian():
     return render_template('home.html', 
                            jobs=JOBS, 
-                           company_name='Jovian')
+                           company_name='initial')
 
 @app.route("/api/jobs")
 def list_jobs():
